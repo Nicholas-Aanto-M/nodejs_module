3481bdcbd508902745c94afb2bcbae58f8893c27
Nicholas-Aanto-M <nicholasanto1@gmail.com>
2022-01-31 21:26:44 +0530

    Added a different methods to implement function

---
+++ app.js
@@ -0,0 +1,15 @@
+// function addNumber(a, b) {
+//   return a + b;
+// }
+
+// const addNumber = function (a, b) {
+//   return a + b;
+// };
+
+const addNumber = (a, b) => {
+  return a + b;
+};
+
+const addValue = addNumber(12, 10);
+
+console.log(addValue);
