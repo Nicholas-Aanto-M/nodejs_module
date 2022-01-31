f2ce47fe27827da370cdc9658409beaa8e882110
Nicholas-Aanto-M <nicholasanto1@gmail.com>
2022-01-31 23:28:53 +0530

    Complitted Using Packages

--- using_packages/app.js
+++ using_packages/app.js
@@ -1,3 +1,10 @@
+const moment = require("moment");
+
 const name = "Hello";
 
 console.log(name);
+
+const date = moment().format("DD/MM/YYYY hh:mm a");
+const future = moment().add(3, "days");
+
+console.log(future.format("DD/MM/YYYY"));
